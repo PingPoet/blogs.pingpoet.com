4fa2cfa763c25d69ae4543431d463c0d7288ffdd
Scott Willeke <scott@willeke.com>
2018-05-07 19:56:58 -0700

    renaming Scripts to lowercase

--- tolowecase.sh
+++ tolowecase.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
-ROOT=docs/PhilBlog
+ROOT=docs/Scripts
 find "$ROOT" -type l | while read f
 do
    lc=$(echo ${f} | tr A-Z a-z)
-   echo "$f -> ${lc}_"
-   git mv $f ${lc}_
+   git mv -v $f ${lc}_
 done
 
 ROOT=$(echo "$ROOT" | tr A-Z a-z)
 find "$ROOT" -type l | while read f
 do
    lc=$(echo ${f} | tr A-Z a-z)
-   echo "${lc} -> ${lc%?}"
-   git mv ${lc} ${lc%?}
+   git mv -v ${lc} ${lc%?}
 done
